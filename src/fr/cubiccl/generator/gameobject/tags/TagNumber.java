245f07ffa18e31b13f19c10213147ae0501d66a9
Cubiccl <ccl.clement@hotmail.fr>
2016-12-06 12:53:31 +0100

    Fixes

--- src/fr/cubiccl/generator/gameobject/tags/TagNumber.java
+++ src/fr/cubiccl/generator/gameobject/tags/TagNumber.java
@@ -4,9 +4,9 @@ import fr.cubiccl.generator.gameobject.templatetags.TemplateNumber;
 
 public class TagNumber extends TagValue
 {
-	public static final int INTEGER = 0, BYTE = 1, SHORT = 2, FLOAT = 3, DOUBLE = 4;
+	public static final int INTEGER = 0, BYTE = 1, SHORT = 2, FLOAT = 3, DOUBLE = 4, BYTE_BOOLEAN = 5;
 	private static final String[] SUFFIX =
-	{ "", "b", "s", "f", "d" };
+	{ "", "b", "s", "f", "d", "b" };
 
 	public TagNumber(TemplateNumber template, String value)
 	{
