0e1e721274c449646cdf538ee081eb47e27a0e5b
Cubiccl <ccl.clement@hotmail.fr>
2017-04-12 15:15:29 +0200

    Release v2.5.2

--- src/fr/cubiccl/generator/gameobject/baseobjects/DefaultAdvancement.java
+++ src/fr/cubiccl/generator/gameobject/baseobjects/DefaultAdvancement.java
@@ -9,28 +9,30 @@ import fr.cubiccl.generator.utils.Text;
 
 public class DefaultAdvancement extends BaseObject
 {
+	public final String category;
 	public final String[] criteria;
 	public final String id;
 	public final Item item;
 
-	public DefaultAdvancement(String id, Item item, String... criteria)
+	public DefaultAdvancement(String id, Item item, String category, String... criteria)
 	{
 		this.id = id;
 		this.item = item;
 		this.criteria = criteria;
+		this.category = category;
 		if (id != null) ObjectRegistry.advancements.register(this);
 	}
 
 	@Override
 	public String id()
 	{
-		return "minecraft:story/" + this.id;
+		return "minecraft:" + this.category + "/" + this.id;
 	}
 
 	@Override
 	public Text name()
 	{
-		return new Text("advancement." + this.id);
+		return new Text("advancement." + this.category + "." + this.id);
 	}
 
 	@Override
@@ -43,6 +45,7 @@ public class DefaultAdvancement extends BaseObject
 	public Element toXML()
 	{
 		Element root = new Element("advancement");
+		root.setAttribute("category", this.category);
 		root.setAttribute("id", this.id);
 		root.setAttribute("item", this.item.id().substring("minecraft:".length()));
 		if (this.criteria.length != 1 || !("minecraft:" + this.criteria[0]).equals(this.item.id())) for (String criterion : this.criteria)
