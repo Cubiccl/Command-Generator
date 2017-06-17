4840e657611765c9f2d7f9dea21df442d50e1850
Cubiccl <ccl.clement@hotmail.fr>
2017-06-17 16:03:39 +0200

    Added if/unless to /function

--- src/fr/cubiccl/generator/command/CommandFunction.java
+++ src/fr/cubiccl/generator/command/CommandFunction.java
@@ -1,21 +1,35 @@
 package fr.cubiccl.generator.command;
 
 import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import fr.cubiccl.generator.gameobject.target.Target;
+import fr.cubiccl.generator.gui.component.combobox.OptionCombobox;
 import fr.cubiccl.generator.gui.component.panel.CGPanel;
+import fr.cubiccl.generator.gui.component.panel.gameobject.PanelTarget;
 import fr.cubiccl.generator.gui.component.textfield.CGEntry;
 import fr.cubiccl.generator.utils.CommandGenerationException;
 import fr.cubiccl.generator.utils.Text;
 
-public class CommandFunction extends Command
+public class CommandFunction extends Command implements ActionListener
 {
+	private OptionCombobox comboboxMode;
 	private CGEntry entryMessage;
+	private PanelTarget panelTarget;
 
 	public CommandFunction()
 	{
-		super("function", "function <path>", 2);
+		super("function", "function <path> [<if|unless> <target>]", 2, 4);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		this.panelTarget.setVisible(!this.comboboxMode.getValue().equals("normal"));
+		this.updateTranslations();
 	}
 
 	@Override
@@ -27,6 +41,10 @@ public class CommandFunction extends Command
 		panel.add(this.labelDescription(), gbc);
 		++gbc.gridy;
 		panel.add((this.entryMessage = new CGEntry(new Text("function.path"), new Text("function.path"))).container, gbc);
+		++gbc.gridy;
+		panel.add(this.comboboxMode = new OptionCombobox("function", "normal", "if", "unless"), gbc);
+		++gbc.gridy;
+		panel.add(this.panelTarget = new PanelTarget(PanelTarget.ALL_ENTITIES), gbc);
 
 		this.entryMessage.addKeyListener(new KeyListener()
 		{
@@ -44,6 +62,9 @@ public class CommandFunction extends Command
 			public void keyTyped(KeyEvent e)
 			{}
 		});
+		this.comboboxMode.addActionListener(this);
+		this.panelTarget.setVisible(false);
+		this.panelTarget.addArgumentChangeListener(this);
 
 		return panel;
 	}
@@ -51,19 +72,31 @@ public class CommandFunction extends Command
 	@Override
 	protected Text description()
 	{
-		return this.defaultDescription();
+		if (this.comboboxMode.getValue().equals("normal")) return this.defaultDescription();
+		return new Text("command." + this.id + "." + this.comboboxMode.getValue()).addReplacement("<target>", this.panelTarget.displayTarget());
 	}
 
 	@Override
 	public String generate() throws CommandGenerationException
 	{
-		return this.id + " " + this.entryMessage.getText();
+		if (this.comboboxMode.getValue().equals("normal")) return this.id + " " + this.entryMessage.getText();
+		return this.id + " " + this.entryMessage.getText() + " " + this.comboboxMode.getValue() + " " + this.panelTarget.generate().toCommand();
 	}
 
 	@Override
 	protected void readArgument(int index, String argument, String[] fullCommand) throws CommandGenerationException
 	{
+		// function <path> [<if|unless> <target>]
 		if (index == 1) this.entryMessage.setText(argument);
+		if (index == 2) this.comboboxMode.setValue(argument);
+		if (index == 3) this.panelTarget.setupFrom(new Target().fromString(argument));
+	}
+
+	@Override
+	protected void resetUI()
+	{
+		this.comboboxMode.setValue("normal");
+		this.panelTarget.setVisible(false);
 	}
 
 }
